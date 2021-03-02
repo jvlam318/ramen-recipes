244b1e0f14617cd61947848616d905f4f4661ccd
Joseph Lam <josephlam@Josephs-MacBook-Pro.local>
2021-03-02 17:24:06 +0000

    changed scripts

--- server.js
+++ server.js
@@ -3,17 +3,17 @@
 // require express and bodyParser
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors')
-const path = require("path")
+const cors = require('cors');
+const path = require("path");
 
 // Import DB Connection
 require("./config/db");
 
-// create express app
-const app = express();
-
 // define port to run express app
 const port = process.env.PORT || 4000;
+
+// create express app
+const app = express();
 
 // use bodyParser middleware on express app
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
 const routes = require('./routes/recipeRoutes'); //importing route
 routes(app);
 
+// Get your Express app to serve up your React app
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
